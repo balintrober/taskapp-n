d62beabeb5555964b61c2405c8edc1b809ac33f1
Bálint Róbert <balint.robert78@gmail.com>
2022-02-21 20:29:28 +0100

    HTML elem tartalmának tesztje

--- tests/unit/Task_html.spec.js
+++ tests/unit/Task_html.spec.js
@@ -2,17 +2,16 @@ import { mount } from '@vue/test-utils'
 import Task from '@/components/Task.vue'
 
 describe('Task komponens egységtesztje', () => {
-  test('Vue példány-e', () => {
-    
+  test('HTML elem tartalmazza-e a feladat nevét', () => {
+    const taskName = 'Feladat';
     const wrapper = mount(Task, {
       propsData: {
         task: {
-          title: 'Task 1',
+          title: taskName,
           completed: false
         } 
       }
     });
-    const t = wrapper.findComponent(Task)
-    expect(t.exists()).toBe(true)    
-  });
+    expect(wrapper.html()).toContain(taskName);   
+    });
 });
